60188a26432278548201543f19962369fa47fa37
Matthias Wagler <wagler@intuity.de>
2017-09-21 00:04:48 +0200

    Update dependencies.

--- src/server/routes/renderPage.js
+++ src/server/routes/renderPage.js
@@ -31,15 +31,15 @@ const renderPage = function () {
 
       if (isProductionMode) {
         // Because this code is made public, we need to keep the spaces to make it look good.
-        gaCode = `<script>
-              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-              ga('create', 'UA-36681683-5', 'auto');
-              ga('send', 'pageview');
-            </script>`;
+        // gaCode = `<script>
+        //       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        //       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        //       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        //       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        //
+        //       ga('create', 'UA-36681683-5', 'auto');
+        //       ga('send', 'pageview');
+        //     </script>`;
       }
 
       res.send(`<!doctype html>
