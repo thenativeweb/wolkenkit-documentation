9ad5c5761cc3157ea4fd3cd84ebab2d5769773ad
Golo Roden <golo.roden@thenativeweb.io>
2017-11-27 12:58:06 +0100

    Fix license issue.

--- roboter.js
+++ roboter.js
@@ -84,8 +84,8 @@ roboter.
         // MIT, see https://github.com/stevenvachon/nopter/blob/bb0478aa64b6a1a156c28cfc51e94788617d6c0f/package.json
         nopter: '0.3.0',
 
-        // BSD-2-Clause, see https://github.com/facebook/regenerator/blob/85c9e43331576be96e5dcc61757995397ab15b77/LICENSE
-        'regenerator-transform': '0.9.11',
+        // BSD-2-Clause, see https://github.com/facebook/regenerator/blob/30d34536b9e3f7a2873b04a16ec66fec9c8246f6/LICENSE
+        'regenerator-transform': '0.10.1',
 
         // BSD-2-Clause, see https://github.com/jviereck/regjsparser/blob/0.1.5/LICENSE.BSD
         regjsparser: '0.1.5',
